a7c3540a23566ec9f5e2f2ef01bb09921d70eeb8
Alba De biase <debiasealba@gmail.com>
2020-10-14 21:10:00 +0200

    mail non trovata

--- main.js
+++ main.js
@@ -1,11 +1,34 @@
 // Creare un array con un insieme di indirizzi email.
 // Chiedere all'utente il suo indirizzo email e verificare se è nella lista (tramite un ciclo for).
 // Comunicare quindi un messaggio appropriato (sei in lista/non sei in lista)
-var email = ["pippo@gmail.com", "pluto@gmail.com", "topolino@gmail.com", "topolino@gmail.com"];
-console.log(email[0]);
-console.log(email[1]);
-console.log(email[2]);
-console.log(email[3]);
+
+// Creare un array con un insieme di indirizzi email.
+var email = ["pippo@gmail.com", "pluto@gmail.com", "topolino@gmail.com", "paperino@gmail.com"];
+// console.log(email[0]);
+// console.log(email[1]);
+// console.log(email[2]);
+// console.log(email[3]);
+
+// Chiedere all'utente il suo indirizzo email
 var tuaEmail = prompt ("inserire indirizzo mail");
 console.log(tuaEmail);
-email.push(tuaEmail);
+
+// verificare se è nella lista
+
+var isFound = false;
+for(var i = 0; i < email.length; i++) {
+    console.log(email[i]);
+// var elementoArray = email[i];
+
+if (email == tuaEmail) {
+// console.log("tuaEmail è presente");
+isFound = true;
+
+    }
+}
+
+if (isFound) {
+console.log("tuaEmail è stata trovata");
+} else {
+console.log("tuaEmail NON è stata trovata");
+}
